bcb778180fc7d109e225e580e47081422f4bf862
Guus Waals <_@guusw.nl>
2023-03-19 21:30:28 +0800

    Add fixes & notes about android building

--- Android.cmake
+++ Android.cmake
@@ -5,7 +5,17 @@
 # ANDROID_TOOLS_VERSION: Version of the android build tools to use (e.g. "32.0.0")
 # JAVA_HOME:             Path to the JDK (e.g. "A:\\Java\\jdk-11)
 
+# Notes about java versions:
+# For compilation, the highest supported version depends on what R8 supports
+# which seems to be version 11 in build tools version 32.0.0
+# For running some of the build tools however, a newer version seems to be required as
+# older versions don't have to required signing algorithms
+# so you should manually set the following variables to a newer version (e.g. JDK 17):
+# - Java_JAVA_EXECUTABLE:FILEPATH=C:/jdk-17.0.2/bin/java.exe
+# - Java_KEYTOOL:FILEPATH=C:/jdk-17.0.2/bin/keytool.exe
+
 option(ANDROID_DEBUG "Debug mode")
+
 if(${ANDROID_DEBUG} OR CMAKE_BUILD_TYPE MATCHES "Debug")
   set(_ANDROID_DEBUG ON)
 endif()
@@ -30,12 +40,15 @@ option(ANDROID_SDK_ROOT "Path to the android SDK root folder")
 android_sdk_from_env("ANDROID_HOME")
 android_sdk_from_env("ANDROID_SDK_ROOT")
 android_sdk_from_env("ANDROID_SDK")
+
 if(NOT ANDROID_SDK_ROOT)
   message(FATAL_ERROR "ANDROID_SDK_ROOT not set")
 endif()
+
 message(STATUS "Using Android SDK: ${ANDROID_SDK_ROOT}")
 
 option(ANDROID_TOOLS_VERSION "Version of the build tools to use")
+
 if(NOT ANDROID_TOOLS_VERSION)
   message(FATAL_ERROR "ANDROID_TOOLS_VERSION not set")
 endif()
@@ -62,15 +75,18 @@ find_program(Java_KEYTOOL NAMES "keytool" PATHS ${JDK_PATH} REQUIRED NO_DEFAULT_
 set(DEBUG_KEYSTORE_PATH ${CMAKE_BINARY_DIR}/debug.keystore)
 set(DEBUG_KEYSTORE_KEY androiddebugkey)
 set(DEBUG_KEYSTORE_PASS android)
+
 function(ensure_debug_keystore)
   if(NOT EXISTS ${DEBUG_KEYSTORE_PATH})
     message(STATUS "Generating android debug keystore")
     execute_process(COMMAND ${Java_KEYTOOL} -genkey -v -dname "CN=debug" -keystore ${DEBUG_KEYSTORE_PATH} -storepass ${DEBUG_KEYSTORE_PASS} -keypass ${DEBUG_KEYSTORE_PASS} -alias ${DEBUG_KEYSTORE_KEY} -keyalg RSA -keysize 2048 -validity 10000)
   endif()
 endfunction()
+
 ensure_debug_keystore()
 
 set(ANDROID_SCRIPT_SRC_DIR "${CMAKE_CURRENT_LIST_DIR}/scripts")
+
 function(configure_android_debug_scripts APK_PATH TARGET_NAME)
   message(STATUS "configure_android_debug_scripts(${APK_PATH})")
   get_filename_component(APK_DIR "${APK_PATH}" DIRECTORY)
@@ -96,13 +112,13 @@ endfunction()
 function(add_android_package)
   set(OPTS)
   set(ARGS
-    NAME            # (Required) The name of the package to build
-    RES_PATH        # (Required) Path to android resources
-    MANIFEST        # (Required) Android manifest path
+    NAME # (Required) The name of the package to build
+    RES_PATH # (Required) Path to android resources
+    MANIFEST # (Required) Android manifest path
   )
   set(MULTI_ARGS
-    SOURCES            # (Required) Android java sources
-    LIB_TARGETS        # Native libs to include
+    SOURCES # (Required) Android java sources
+    LIB_TARGETS # Native libs to include
   )
   cmake_parse_arguments(APK "${OPTS}" "${ARGS}" "${MULTI_ARGS}" ${ARGN})
   message(STATUS "add_android_package(${APK_NAME})")
@@ -116,11 +132,18 @@ function(add_android_package)
   )
   set(JAR_PATH "${JAR_NAME}.jar")
 
+  if(ANDROID_PLATFORM MATCHES "android-([0-9]+)")
+    set(ANDROID_PLATFORM_NR ${CMAKE_MATCH_1})
+    message(STATUS "  Targeting android version: ${ANDROID_PLATFORM_NR}")
+  else()
+    message(FATAL "  Invalid android platform format: ${ANDROID_PLATFORM}")
+  endif()
+
   set(CLASSES_DEX_PATH ${CMAKE_CURRENT_BINARY_DIR}/${APK_NAME}-dex)
   file(MAKE_DIRECTORY ${CLASSES_DEX_PATH})
   add_custom_command(
     OUTPUT ${CLASSES_DEX_PATH}/classes.dex
-    COMMAND ${Java_JAVA_EXECUTABLE} -cp ${ANDROID_D8_JAR} com.android.tools.r8.D8 ${JAR_PATH} --output ${CLASSES_DEX_PATH}
+    COMMAND ${Java_JAVA_EXECUTABLE} -cp ${ANDROID_D8_JAR} com.android.tools.r8.D8 ${JAR_PATH} --min-api ${ANDROID_PLATFORM_NR} --output ${CLASSES_DEX_PATH}
     DEPENDS ${JAR_PATH}
     COMMENT "Dexing"
     USES_TERMINAL
@@ -135,6 +158,7 @@ function(add_android_package)
   file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/${COMPILED_RES_DIR_PATH})
 
   file(GLOB_RECURSE RESOURCE_FILES "${APK_RES_PATH}/*")
+
   foreach(RES_FILE ${RESOURCE_FILES})
     file(RELATIVE_PATH REL_RES_PATH ${APK_RES_PATH} ${RES_FILE})
     get_filename_component(RES_NAME_WE ${RES_FILE} NAME_WE)
@@ -143,6 +167,7 @@ function(add_android_package)
     string(REPLACE "/" "_" COMPILED_RES_NAME ${REL_RES_PATH})
     get_filename_component(RES_EXT ${COMPILED_RES_NAME} LAST_EXT)
     get_filename_component(COMPILED_RES_NAME_WLE ${COMPILED_RES_NAME} NAME_WLE)
+
     if(COMPILED_RES_NAME MATCHES "^values_.*\.xml$")
       set(COMPILED_RES_NAME "${COMPILED_RES_NAME_WLE}.arsc.flat")
     else()
@@ -172,6 +197,7 @@ function(add_android_package)
     file(REAL_PATH ${RES} RES_NATIVE BASE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
     list(APPEND RESOURCES_NATIVE ${RES_NATIVE})
   endforeach()
+
   file(TO_NATIVE_PATH "res-compiled" RES_PATH_NATIVE)
 
   # Link resources into apk
@@ -191,11 +217,12 @@ function(add_android_package)
   set(NATIVE_LIB_PATH "lib/${ANDROID_ABI}")
 
   # Create directories
-  file(REMOVE_RECURSE  ${APK_STAGING_PATH})
+  file(REMOVE_RECURSE ${APK_STAGING_PATH})
   file(MAKE_DIRECTORY ${APK_STAGING_PATH}/${NATIVE_LIB_PATH})
 
   # Merge code and resources
   set(APK_PATH ${CMAKE_CURRENT_BINARY_DIR}/${APK_NAME}.unaligned.apk)
+
   foreach(LIB_TARGET ${APK_LIB_TARGETS})
     list(APPEND COPY_NATIVE_LIBS_COMMAND
       COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:${LIB_TARGET}> ${NATIVE_LIB_PATH}
@@ -209,7 +236,6 @@ function(add_android_package)
   else()
     set(ARCHIVE_CMDLINE ${SEVEN_ZIP} a -tzip ${APK_PATH} *)
   endif()
-
 
   add_custom_command(
     OUTPUT ${APK_PATH}
@@ -235,6 +261,7 @@ function(add_android_package)
   # Final APK path
   set(BUILD_TYPE_UPPER ${CMAKE_BUILD_TYPE})
   string(TOUPPER "${BUILD_TYPE_UPPER}" BUILD_TYPE_UPPER)
+
   if(CMAKE_RUNTIME_OUTPUT_DIRECTORY_${BUILD_TYPE_UPPER})
     set(APK_OUT_DIR ${CMAKE_RUNTIME_OUTPUT_DIRECTORY_${BUILD_TYPE_UPPER}})
   elseif(CMAKE_RUNTIME_OUTPUT_DIRECTORY)
@@ -242,6 +269,7 @@ function(add_android_package)
   else()
     set(APK_OUT_DIR ${CMAKE_CURRENT_BINARY_DIR})
   endif()
+
   set(APK_PATH ${APK_OUT_DIR}/${APK_NAME}.apk)
 
   # apksigner
